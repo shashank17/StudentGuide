d9ed8a5c3b720636c1e90dcc81e29b8fa1ad3bba
quentonLaRoe <quentonLaRoe@github.com>
2016-12-04 12:41:20 -0600

    New methods
    Added new, empty methods, that will be used for constraints.

--- StudentGuide/src/code/ConstraintChecker.java
+++ StudentGuide/src/code/ConstraintChecker.java
@@ -23,12 +23,26 @@ public class ConstraintChecker {
 	}
 	//Req GENED_TIER1_WSM - first semester
 	//Req GENED_TIER1_LST - first semester
+	public boolean firstSemester(Semester semester){
+		
+		return true;
+	}
 	//Req GENED_TIER1_MAT - first or second semester
 	//Req GENED_TIER1_FLG - first or second semester
-	
+	public boolean freshman(ArrayList<Semester>semester){
+		return true;
+	}
 	//Req GENED_UL_OUTSIDE_DIV - because its an upper level, probably junior or senior year
 	//Req GENED_TIER3_INTEG - probably senior year, maybe junior
-	
+	public boolean upperClass(ArrayList<Semester>semester){
+		return true;
+	}
 	//Courses CSC_327, CSC_350, ITY_351, PH_MAJOR_ELECT1_2 usually taken together. Fall semester, junior or senior year.
+	public boolean upperClassFall(ArrayList<Semester>semester){
+		return true;
+	}
 	//check pre-requisites and co-requisites for other courses
+	public boolean pre_co_req(ArrayList<Semester>semester, Course course){
+		return true;
+	}
 }
