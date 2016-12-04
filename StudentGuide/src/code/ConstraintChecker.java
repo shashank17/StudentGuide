aa5d4e90b01194cd7c13349ea0d21e9758537eda
Sue Brownawell <github@brownawell.us>
2016-12-03 18:03:25 -0600

    Pseudocode constraints added
    Pseudocode constraints added

--- StudentGuide/src/code/ConstraintChecker.java
+++ StudentGuide/src/code/ConstraintChecker.java
@@ -1,5 +1,17 @@
 package code;
 
 public class ConstraintChecker {
+	//12 - 19 credits per semester
+	//122 credits or more total
 	
+	//Req GENED_TIER1_WSM - first semester
+	//Req GENED_TIER1_LST - first semester
+	//Req GENED_TIER1_MAT - first or second semester
+	//Req GENED_TIER1_FLG - first or second semester
+	
+	//Req GENED_UL_OUTSIDE_DIV - because its an upper level, probably junior or senior year
+	//Req GENED_TIER3_INTEG - probably senior year, maybe junior
+	
+	//Courses CSC_327, CSC_350, ITY_351, PH_MAJOR_ELECT1_2 usually taken together. Fall semester, junior or senior year.
+	//check pre-requisites and co-requisites for other courses
 }
