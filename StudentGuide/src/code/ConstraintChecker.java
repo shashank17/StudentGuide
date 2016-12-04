0b625fe67c3fee712ee881e90467b4df64cda5c2
quentonLaRoe <quentonLaRoe@github.com>
2016-12-04 12:23:07 -0600

    constraint checker
    Added in a couple methods that make sure credit hours are met.

--- StudentGuide/src/code/ConstraintChecker.java
+++ StudentGuide/src/code/ConstraintChecker.java
@@ -1,9 +1,26 @@
 package code;
 
+import java.util.ArrayList;
+
 public class ConstraintChecker {
-	//12 - 19 credits per semester
+
+	//method that will make sure there are between 12 and 19 credit hours
+	public boolean semesterHours(ArrayList<Semester> semester){
+		for(Semester s:semester){
+			if(s.getTotalCredits() > 19 || s.getTotalCredits() < 12)
+				return false;
+		}
+		return true;
+	}
 	//122 credits or more total
-	
+	public boolean creditHours(ArrayList<Semester>semester){
+		int total = 0;
+		for(Semester s:semester)
+			total += s.getTotalCredits();
+		if(total < 122)
+			return false;
+		return true;
+	}
 	//Req GENED_TIER1_WSM - first semester
 	//Req GENED_TIER1_LST - first semester
 	//Req GENED_TIER1_MAT - first or second semester
